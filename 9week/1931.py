ecafb55337807e7f1858694d6c783faf1ca43355
gwonjihun <aspire51@naver.com>
2022-11-06 21:41:17 +0900

    '1931sol'

--- 9week/1931.py
+++ 9week/1931.py
@@ -6,11 +6,11 @@ for i in range(n):
     arr[i][1]=e
 
 arr.sort(key= lambda x:(x[1],x[0]))
-print(arr)
 cnt= 1 
 end = arr[0][1]
 
 for i in range(1,n):
-    if end<arr[i][0]:
-        pass
-print(arr)
+    if end<=arr[i][0]:
+        cnt += 1
+        end = arr[i][1]
+print(cnt)
