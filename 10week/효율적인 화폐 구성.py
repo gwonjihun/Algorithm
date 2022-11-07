f28059b9ae69375e5dfa62d8760df5beda1dc314
gwonjihun <aspire51@naver.com>
2022-11-07 21:47:47 +0900

    '10주차알분과제1'

--- 10week/효율적인 화폐 구성.py
+++ 10week/효율적인 화폐 구성.py
@@ -2,12 +2,19 @@ n , m = map(int,input().split())
 
 arr = [ int(input()) for _ in range(n)]
 
-dp = [10001]*10001
+dp = [10001]* (m+1)
  # 최소값을 구하므로 dp배열에 최대값 이상의 값으로 저장
 dp[0] = 0
-for i in range(1,n): #arr 배열에 있는 값들만 활용하기 위해서
-    for j in range(arr[i],m+1): 
-        if dp[j - arr[i]] != 10001:
-            dp[j] = min(dp[j],dp[j-arr[i]]+1)
+for i in range(n):
+    for j in range(1,m+1): 
+        if j-arr[i]>=0:
+            # j-arr[i]가 -인경우 배열의 인덱스 접근이 불가하게 설정해야함
+            if dp[j - arr[i]] != 10001:
+                # dp[j-arr[i]]가 존재하는 경우를 확
+                dp[j] = min(dp[j],dp[j-arr[i]]+1)
 
-print(dp[:m+1])
+print(dp)
+if dp[m] ==10001:
+    print(-1)
+else:
+    print(dp[-1])
