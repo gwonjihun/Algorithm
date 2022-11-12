8089472d7731dec1171e7c7a7dae0aeb73d12e73
gwonjihun <aspire51@naver.com>
2022-11-12 16:03:49 +0900

    11_12pm

--- 10week/bae_7576.py
+++ 10week/bae_7576.py
@@ -1,37 +1,33 @@
-T = int(input())
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
 from collections import deque
-def bfs(graph,a,b):
-    que = deque()
-    que.append((a,b))
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+m, n = map(int,input().split())
 
-    while que:
-        x,y = que.popleft()
+arr= [ list(map(int,input().split())) for _ in range(n)]
+s = deque()
+result = 0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1:
+            s.append([i,j])
+
+def bfs():
+    while s:
+        x,y = s.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if (0<= nx < m and 0<= ny < n) and graph[nx][ny]==1:
-                print(nx,ny)
-                graph[nx][ny] =0
-                que.append((nx,ny))
-    return
+            nx, ny = dx[i]+x,dy[i]+y
+
+            if 0<= nx < n and 0<=ny <m and arr[nx][ny]==0:
+                arr[nx][ny]=arr[x][y]+1
+                s.append([nx,ny])
 
 
-for test_case in range(T):
-    m, n, s = map(int,input().split())
-
-    arr = [[0]*n for _ in range(m)]
-    for _ in range(s):
-        x,y = map(int,input().split())
-        arr[x][y] = 1
+bfs()
+for i in arr:
+    for j in i:
+        if j==0:
+            print(-1)
+            exit()
     
-    answer = 0
-    
-    for i in range(m):
-        for j in range(n):
-            if arr[i][j] == 1:
-                bfs(arr,i,j)
-                answer +=1
-    print(answer)
-    
+    result = max(result,max(i))
+print(result-1)
