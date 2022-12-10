97c0f998cfae674d757a65012ebe61c46fa13b1d
gwonjihun <aspire51@naver.com>
2022-12-10 14:56:21 +0900

    1992

---
+++ 13week/백준_1992.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+arr =[list(map(int,input())) for _ in range(n)]
+
+print(arr)
+
+def check_dif(x,y,n):
+    check = arr[x][y]
+    for i in range(x,x+n):
+        for j in range(y,y+n):
+            if check != arr[i][j]:
+                check = -1
+                return -1
+    return check
+
+def check_box(x,y,n):
+    check = check_dif(x,y,n)
+
+    if check == -1:
+        print("(",end="")
+        n = n//2
+        check_box(x,y,n)
+        check_box(x,y+n,n)
+        check_box(x+n,y,n)
+        check_box(x+n,y+n,n)
+        print(")",end="")
+    
+    elif check == 1:
+        print(1,end="")
+    else:
+        print(0,end="")
+
+check_box(0,0,n)
--- 13week/백준_2579_dp.py
+++ 13week/백준_2579_dp.py
@@ -1,12 +1,14 @@
 n = int(input())
-arr = []
-for _ in range(n):
-    a = list(map(int,input().split()))
-    arr.append(a)
+arr = [0]
+dp = [0] * (n+1)
 
-for i in range(1,n):
-    arr[i][0] = min(arr[i-1][1],arr[i-1][2])+arr[i][0]
-    arr[i][1] = min(arr[i-1][0],arr[i-1][2])+arr[i][1]
-    arr[i][2] = min(arr[i-1][0],arr[i-1][1])+arr[i][2]
+for i in range(n):
+    arr.append(int(input()))
 
-print(min(arr[n-1][0],arr[n-1][1],arr[n-1][2]))
+dp[1]= arr[1]
+dp[2]= max(arr[0]+arr[2],arr[1]+arr[2])
+
+for i in range(3,n+1):
+    dp[i]= max(dp[i-2]+arr[i],dp[i-3]+arr[i-1]+arr[i])
+
+print(dp[-1])
