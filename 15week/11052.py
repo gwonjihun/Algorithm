b4fbd09e8a7e9d642a4fc7fc977f8701c58bc1bd
gwonjihun <aspire51@naver.com>
2023-01-17 19:48:46 +0900

    11052

---
+++ 15week/11052.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+arr = [0]+ list(map(int,input().split())) 
+dp = [ 0 for _ in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        if dp[i] < dp[i-j] + arr[j]:
+            dp[i] = dp[i-j] + arr[j]
+print(dp[-1])
