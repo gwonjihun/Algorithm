9fd75c350cebb6ab0a339a551b4cf761262879a2
gwonjihun <aspire51@naver.com>
2023-01-23 21:48:54 +0900

    '11051'

--- 23_3week/11051.py
+++ 23_3week/11051.py
@@ -1,10 +1,10 @@
-# def factorial(i : int):
-#     sum = 1
-#     for j in range(1,i+1):
-#         sum *= j
+def factorial(i : int):
+    sum = 1
+    for j in range(1,i+1):
+        sum *= j
 
-#     return sum
+    return sum
 
-# N, k = map(int,input().split())
+N, k = map(int,input().split())
 
-# print((factorial(N)//factorial(N-k)//factorial(k))%10007)
+print((factorial(N)//factorial(N-k)//factorial(k))%10007)
